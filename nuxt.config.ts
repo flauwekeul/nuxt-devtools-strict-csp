e369a82d2668ad2d056b24fafe8b3ad12b3b0856
Abbe Keultjes <flauwekeul@gmail.com>
2023-10-09 11:49:59 +0200

    add and configure nuxt-security with strict CSP for 'script-src'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-security'],
+  security: {
+    enabled: true,
+    nonce: true,
+    headers: {
+      contentSecurityPolicy: {
+        'script-src': ["'nonce-{{nonce}}'", "'strict-dynamic'"],
+      },
+    },
+  },
 })
